9af38306e64c2fa1413139fe7f62ffd01cbf940d
Luiz Wagner Bertoldi de Oliveira <wagner.bertoldi@192.168.100.7>
2023-06-24 16:06:08 -0300

    fix

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::post('/login', [App\Http\Controllers\AuthController::class, 'login']);
-Route::post('/reset', 'App\Http\Controllers\Auth\ResetPasswordController@reset')->name('password.update');
+//Route::post('/reset', 'App\Http\Controllers\Auth\ResetPasswordController@reset')->name('password.update');
 
 Route::prefix('user')->group(function (){
     Route::post('set', [\App\Http\Controllers\UserController::class, 'set']);
