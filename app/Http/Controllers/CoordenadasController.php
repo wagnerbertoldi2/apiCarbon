337c949e5e244a275c0ebd54b7b243dd0c6c9f7f
Luiz Wagner Bertoldi de Oliveira <wagner.bertoldi@192.168.100.7>
2023-12-28 01:06:09 -0300

    fix

--- app/Http/Controllers/CoordenadasController.php
+++ app/Http/Controllers/CoordenadasController.php
@@ -38,7 +38,7 @@ class CoordenadasController extends Controller{
         $this->setEndereco();
         $endereco = $this->logradouro.", ".$this->num.", ".$this->localidade.", ".$this->uf;
 
-        $enderecoFormatado = "https://nominatim.openstreetmap.org/search?format=json&q=".urlencode("$endereco");
+        $enderecoFormatado = "https://nominatim.openstreetmap.org/search?q=".urlencode("$endereco")."&format=json";
         $resposta = file_get_contents($enderecoFormatado);
         return response()->json($resposta, 401);
         $dados = json_decode($resposta, true);
