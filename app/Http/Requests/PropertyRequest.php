3eaac29b4067fb7d5c2d04454e6498ad229267a9
Wagner Bertoldi <wagner.bertoldi@gmail.com>
2024-01-03 10:28:35 -0300

    fix | property unique

--- app/Http/Requests/PropertyRequest.php
+++ app/Http/Requests/PropertyRequest.php
@@ -11,12 +11,21 @@ class PropertyRequest extends FormRequest{
     }
 
     public function rules(){
+        $propertyId= $this->route('property');
+
         return [
             'Name' => 'required|string',
             'Registration' => 'required|string',
             'CEP' => 'required|string',
             'City' => 'required|string',
-            'Number' => 'required|string',
+            'Number' => [
+                'required',
+                'string',
+                Rule::unique('properties')
+                    ->where('CEP', $this->input('CEP'))
+                    ->where('Complement', $this->input('Complement'))
+                    ->ignore($propertyId),
+                ],
             'Complement' => 'nullable|string',
             'NumberOfPeoples' => 'required|integer',
             'Address' => 'required|string',
