3b5263adc62859ca1859b73c731d01bc8fb8e5b4
Wagner Bertoldi <wagner.bertoldi@gmail.com>
2024-01-03 10:13:40 -0300

    fix

--- app/Http/Controllers/EmissionController.php
+++ app/Http/Controllers/EmissionController.php
@@ -32,7 +32,7 @@ class EmissionController extends Controller{
     public function get(Request $request){
         if($request->has('EmissionSourceId') && $request->has('propertyId')){
             $emission = DB::table('emission AS E')
-                ->select('E.id', 'E.Amount', 'E.Attachment as file', DB::raw('concat("'.url('/').'/files/", E.Attachment) as urlDoComprovante'), 'P.Name as period', 'PP.Name as property', 'ES.Name as factor', DB::raw('if(P.`Name`="Anual",E.Year,if(P.`Name`="Mensal",concat(E.`Month`,"/",E.Year),E.Semester)) as periodRef'), 'E.created_at')
+                ->select('E.id', 'E.Amount', 'E.Attachment as file', DB::raw('concat("'.url('/').'/files/", E.Attachment) as urlDoComprovante'), 'P.Name as period', 'PP.Name as property', 'ES.Name as factor', DB::raw('if(P.`Name`="Anual",E.Year,if(P.`Name`="Mensal",concat(E.`Month`,"/",E.Year),concat(E.Semester,"/",E.Year))) as periodRef'), 'E.created_at')
                 ->leftJoin('emissionsource AS ES', 'ES.id', '=', 'E.EmissionSourceId')
                 ->leftJoin('period AS P', 'P.id', '=', 'ES.PeriodId')
                 ->leftJoin('property AS PP', 'PP.id', '=', 'ES.PropertyId')
@@ -43,7 +43,7 @@ class EmissionController extends Controller{
             return response()->json($emission, 200);
         } elseif($request->has('EmissionSourceId')){
             $emission = DB::table('emission as E')
-                ->select('E.id', 'E.Amount', 'E.Attachment as file', DB::raw('concat("'.url('/').'/files/", E.Attachment) as urlDoComprovante'), 'P.Name as period', 'PP.Name as property', 'S.Name as factor', DB::raw('if(P.`Name`="Anual",E.Year,if(P.`Name`="Mensal",concat(E.`Month`,"/",E.Year),E.Semester)) as periodRef'), 'E.created_at')
+                ->select('E.id', 'E.Amount', 'E.Attachment as file', DB::raw('concat("'.url('/').'/files/", E.Attachment) as urlDoComprovante'), 'P.Name as period', 'PP.Name as property', 'S.Name as factor', DB::raw('if(P.`Name`="Anual",E.Year,if(P.`Name`="Mensal",concat(E.`Month`,"/",E.Year),concat(E.Semester,"/",E.Year))) as periodRef'), 'E.created_at')
                 ->leftJoin('emissionsource as S', 'S.id', '=', 'E.EmissionSourceId')
                 ->leftJoin('period as P', 'P.id', '=', 'S.PeriodId')
                 ->leftJoin('property as PP', 'PP.id', '=', 'S.PropertyId')
@@ -53,7 +53,7 @@ class EmissionController extends Controller{
             return response()->json($emission, 200);
         } else {
             $emission = DB::table('emission as E')
-                ->select('E.id', 'E.Amount', 'E.Attachment as file', 'P.Name as period', DB::raw('concat("'.url('/').'/files/", E.Attachment) as urlDoComprovante'), 'PP.Name as property', 'S.Name as factor', DB::raw('if(P.`Name`="Anual",E.Year,if(P.`Name`="Mensal",concat(E.`Month`,"/",E.Year),E.Semester)) as periodRef'), 'E.created_at')
+                ->select('E.id', 'E.Amount', 'E.Attachment as file', 'P.Name as period', DB::raw('concat("'.url('/').'/files/", E.Attachment) as urlDoComprovante'), 'PP.Name as property', 'S.Name as factor', DB::raw('if(P.`Name`="Anual",E.Year,if(P.`Name`="Mensal",concat(E.`Month`,"/",E.Year),concat(E.Semester,"/",E.Year))) as periodRef'), 'E.created_at')
                 ->leftJoin('emissionsource as S', 'S.id', '=', 'E.EmissionSourceId')
                 ->leftJoin('period as P', 'P.id', '=', 'S.PeriodId')
                 ->leftJoin('property as PP', 'PP.id', '=', 'S.PropertyId')
