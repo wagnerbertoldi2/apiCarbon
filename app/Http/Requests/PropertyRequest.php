7b85411714eeb682a14ee6e62ade5de7869807e5
Wagner Bertoldi <wagner.bertoldi@gmail.com>
2024-01-03 10:30:55 -0300

    fix | property unique

--- app/Http/Requests/PropertyRequest.php
+++ app/Http/Requests/PropertyRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Validation\Rule;
 
 class PropertyRequest extends FormRequest{
     public function authorize(){
