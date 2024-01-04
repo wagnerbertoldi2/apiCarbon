33df35b8ef644b18bce439d5b4bef9dc27efcf6e
Wagner Bertoldi <wagner.bertoldi@gmail.com>
2024-01-04 00:40:12 -0300

    fix

--- app/Http/Controllers/EmissionFactorController.php
+++ app/Http/Controllers/EmissionFactorController.php
@@ -10,6 +10,16 @@ class EmissionFactorController extends Controller{
         if($request->has('id')){
             $emissionFactor = EmissionFactorModel::where('id', $request->id)->get();
             return response()->json($emissionFactor);
+        } elseif($request->has('idproperty')) {
+            $idproperty = $request->idproperty;
+
+            return DB::table('emissionfactor')
+                ->whereNotIn('id', function ($query) use ($idproperty) {
+                    $query->select('EmissionFactorId')
+                        ->from('emissionsource')
+                        ->where('PropertyId', $idproperty);
+                })
+                ->get();
         } else {
             $emissionFactor = EmissionFactorModel::all();
             return response()->json($emissionFactor);
