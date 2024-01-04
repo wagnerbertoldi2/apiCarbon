ae431da3206ffc1fc5327c108c3d1aa6ade6386b
Wagner Bertoldi <wagner.bertoldi@gmail.com>
2024-01-04 18:28:16 -0300

    create getList

--- app/Http/Controllers/EmissionController.php
+++ app/Http/Controllers/EmissionController.php
@@ -75,7 +75,7 @@ class EmissionController extends Controller{
             ->where('E.Year', '=', $year)
             ->get();
 
-        return response()->json([$result,$idProperty,$year], 201);
+        return response()->json([$result[0]], 201);
     }
 
     public function update(Request $request){
