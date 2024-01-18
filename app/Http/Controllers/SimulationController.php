4cd7ab8cc3873267992fdaa44b622eee7c5a340e
Wagner Bertoldi <wagner.bertoldi@gmail.com>
2024-01-18 00:22:02 -0300

    teste

--- app/Http/Controllers/SimulationController.php
+++ app/Http/Controllers/SimulationController.php
@@ -64,7 +64,7 @@ class SimulationController extends Controller{
     public function getArraySimulation($functionFactor, $lat, $lon, $emissionFactorID, $regionID, $period, $valueFactor, $ano, $mes, $semestre){
         $factorCalculado= $this->$functionFactor($valueFactor);
         $calcDario= $this->calcDiario($factorCalculado, $period, $ano, $mes, $semestre);
-        return $calcDario;
+        return [$factorCalculado, $period, $ano, $mes, $semestre];
 
         $dias= $calcDario[1];
         $amount= $calcDario[0];
