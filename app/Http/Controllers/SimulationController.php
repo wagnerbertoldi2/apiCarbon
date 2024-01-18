f2a99ee6cabf15c00f90f81e36abeacda7660f88
Wagner Bertoldi <wagner.bertoldi@gmail.com>
2024-01-18 00:06:35 -0300

    fix

--- app/Http/Controllers/SimulationController.php
+++ app/Http/Controllers/SimulationController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\EmissionFactorModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
+use DataTime;
 
 class SimulationController extends Controller{
     private $factors;
