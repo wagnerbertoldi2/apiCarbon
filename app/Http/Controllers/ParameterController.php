36129a466001864212cba5ed0f48c4ca9cf49113
CapittalX <126474183+capittalx@users.noreply.github.com>
2024-04-27 17:16:50 -0300

    fix paramter

--- app/Http/Controllers/ParameterController.php
+++ app/Http/Controllers/ParameterController.php
@@ -5,10 +5,9 @@ namespace App\Http\Controllers;
 use App\Models\Parameter;
 use Illuminate\Http\Request;
 
-class ParameterController extends Controller
-{
-    public function getParameter($id) {
-        $parameter = Parameter::findOrFail($id);
+class ParameterController extends Controller{
+    public function getParameter($paramter) {
+        $parameter = Parameter::where("parameter", $paramter)->first();
         return response()->json($parameter, 200);
     }
 }
