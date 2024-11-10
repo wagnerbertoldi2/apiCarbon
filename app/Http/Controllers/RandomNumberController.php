1fb5055afcf55b25e470fc5ed9133551895f7ef0
CapittalX <126474183+capittalx@users.noreply.github.com>
2024-11-10 20:46:40 -0300

    fix

--- app/Http/Controllers/RandomNumberController.php
+++ app/Http/Controllers/RandomNumberController.php
@@ -14,6 +14,7 @@ class RandomNumberController{
         DB::table('coderesetpassword')->insert([
             "code" => $code,
             "iduser" => $userId,
+            "status" => 2,
             "created_at"=> date("Y-m-d H:i:s")
         ]);
 
