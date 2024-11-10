c050045a8c5f1473f59c44265f0ab9e8f7586221
CapittalX <126474183+capittalx@users.noreply.github.com>
2024-11-10 18:41:17 -0300

    fix

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -131,7 +131,6 @@ class UserController extends Controller{
     public function SendEmailResetPassword(Request $request){
         $email= $request->email;
         $user= DB::table("users")->where("email",$email)->first();
-        dd($user, $request->email);
 
         if(empty($user->email)){
             Mail::to($email)->send(new CodigoNumericoEmail($user));
