d1401e83f85888dc417e5727769f53677cbd31d7
CapittalX <126474183+capittalx@users.noreply.github.com>
2025-02-23 08:21:45 -0300

    fix no delete de emission

--- app/Http/Controllers/EmissionController.php
+++ app/Http/Controllers/EmissionController.php
@@ -288,6 +288,9 @@ class EmissionController extends Controller{
         if($emission->created_at->diffInMinutes() > 15){
             return response()->json(["msg" => "Não é permitido excluir o registro após 15 minutos de sua criação."], 201);
         } else {
+            $nomeArquivo = $emission->Attachment;
+            Storage::disk('local')->delete($nomeArquivo);
+
             $emission->delete();
             DB::connection("mysqlSimulation")->table("simulation")->where("EmissionId", $request->id)->delete();
             return response()->json(true, 200);
